2f39d19ec65ce88171e1838a723ae42183fef327
mlisivick <matt2014!>
2017-11-02 16:51:48 -0300

    Initial Commit

---
+++ StatefulView/StatefulView.h
@@ -0,0 +1,19 @@
+//
+//  StatefulView.h
+//  StatefulView
+//
+//  Created by Matthew Lisivick on 11/2/17.
+//  Copyright © 2017 Redi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StatefulView.
+FOUNDATION_EXPORT double StatefulViewVersionNumber;
+
+//! Project version string for StatefulView.
+FOUNDATION_EXPORT const unsigned char StatefulViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StatefulView/PublicHeader.h>
+
+
