41a95aad57e3086a9c5d536f83d73134eb98e611
mlisivick <matt2014!>
2017-11-02 21:00:22 -0300

    Working Version

--- StatefulView/StatefulView.swift
+++ StatefulView/StatefulView.swift
@@ -7,15 +7,74 @@
 //
 
 import UIKit
+import Foundation
 
-class StatefulView: UIView {
+/// Possible States
+public enum ViewState: String {
+    case loading = "loading"
+    case error = "error"
+    case empty = "empty"
+    case custom = "custom"
+}
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+public class StatefulView: UIView {
+
+    // Default Views
+    private var state: ViewState = ViewState.loading
+    private var views:[ViewState:String] = [:]
+    
+    // Set Views
+    public func setAvailableViews(loadingView: String? = nil, errorView: String? = nil, emptyView: String? = nil, customView: String? = nil){
+        // Loading
+        if let loading = loadingView{
+            views[ViewState.loading] = loading
+        }
+        // Error
+        if let error = errorView{
+            views[ViewState.error] = error
+        }
+        // Empty
+        if let empty = emptyView{
+            views[ViewState.empty] = empty
+        }
+        // Custom
+        if let custom = customView{
+            views[ViewState.custom] = custom
+        }
     }
-    */
-
+    // Set Current State
+    public func setState(state: ViewState){
+        self.state = state
+        self.reloadView()
+    }
+    // Remove Current View
+    private func removeCurrentView(){
+        for view in self.subviews{
+            view.removeFromSuperview()
+        }
+    }
+    
+    // Reload View
+    private func reloadView(){
+        self.removeCurrentView()
+        self.setSelectedView()
+    }
+    // Load View from NIB
+    private func instanceFromNib(name: String) -> UIView? {
+        if let view = UINib(nibName: name, bundle: nil).instantiate(withOwner: nil, options: nil)[0] as? UIView{
+            return view
+        }
+        return nil
+    }
+    // Select View And Redraw
+    private func setSelectedView(){
+        if let stateView = views[self.state]{
+            self.removeCurrentView()
+            if let view = self.instanceFromNib(name: stateView){
+                self.addSubview(view)
+                self.setNeedsLayout()
+                self.setNeedsDisplay()
+            }
+        }
+    }
 }
---
+++ StatefulViewExample/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  StatefulViewExample
+//
+//  Created by Matthew Lisivick on 11/2/17.
+//  Copyright © 2017 Redi. All rights reserved.
+//
+
+import UIKit
+import StatefulView
+
+class ViewController: UIViewController {
+
+    @IBOutlet var segmentSelector: UISegmentedControl!
+    @IBOutlet var statefulView: StatefulView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.segmentSelector.addTarget(self, action: #selector(segmentedControlComparisonChanged), for: .allEvents)
+        self.statefulView.setAvailableViews(loadingView: "LoadingView", errorView: "ErrorView", emptyView: "EmptyView", customView: "MyCustomView")
+        self.statefulView.setState(state: .loading)
+    }
+    
+//    override func viewDidLayoutSubviews() {
+//        state = StatefulView(frame: statefulView.frame)
+//        self.view.addSubview(state)
+//        state.setLoadView()
+//        state.setEmptyView()
+//    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @objc func segmentedControlComparisonChanged(segment: UISegmentedControl) {
+        switch segment.selectedSegmentIndex {
+        case 0:
+            self.statefulView.setState(state: .empty)
+            break
+        case 1:
+            self.statefulView.setState(state: .loading)
+            break
+        case 2:
+            self.statefulView.setState(state: .error)
+            break
+        default:
+            self.statefulView.setState(state: .custom)
+
+        }
+
+    }
+
+}
+
